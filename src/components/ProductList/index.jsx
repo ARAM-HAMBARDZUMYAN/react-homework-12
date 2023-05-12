95db8eb6f8b208bfdc77ca97c684e629b5c3a910
Aram-Hambardzumyan <hambardzumyanaram7@gmail.com>
2023-05-12 22:10:27 +0400

    admin

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -8,35 +8,46 @@ const ProductList = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await axios.get('https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products');
-      dispatch({ type: 'FETCH_PRODUCTS', payload: response.data });
+      try {
+        const response = await axios.get(
+          'https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products'
+        );
+        dispatch({ type: 'FETCH_PRODUCTS', payload: response.data });
+      } catch (error) {
+        console.log('Error fetching products:', error);
+      }
     };
 
     fetchProducts();
   }, [dispatch]);
 
   const handleDeleteProduct = async (productId) => {
-    await axios.delete(`https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products/${productId}`);
-    dispatch({ type: 'DELETE_PRODUCT', payload: productId });
+   
+      await axios.delete(
+        `https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products/${productId}`
+      );
+      dispatch({ type: 'DELETE_PRODUCT', payload: productId });
+   
   };
 
   return (
     <div className="product-list">
       <h2>Products</h2>
-      <ul className="product-items">
-        {products.map((product, index) => (
-          <div key={index} className="product-item">
-            <h1>{product.name}</h1>
+      <div className="product-items">
+        {products.map((product) => (
+          <div key={product._id} className="product-item">
+
+             <h1>{product.name}</h1>
             <p>{product.category}</p>
             <p>{product.description}</p>
             <p>{product.price}</p>
-            
             <button onClick={() => handleDeleteProduct(product._id)}>x</button>
           </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
 
 export default ProductList;
+
--- src/components/ProductModal/index.jsx
+++ src/components/ProductModal/index.jsx
@@ -1,11 +1,10 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 
 const ProductModal = () => {
-    const [showModal, setShowModal] = useState(false);
-  const [formData, setformData] = useState({
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
     name: '',
     description: '',
     price: '',
@@ -16,75 +15,112 @@ const ProductModal = () => {
   const categories = useSelector((state) => state.categoryReducer);
 
   const handleSave = async () => {
-    const response = await axios.post('https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products', formData);
-    dispatch({ type: 'ADD_PRODUCT', payload: response.data });
-    setformData({
-      name: '',
-      description: '',
-      price: '',
-      image: null,
-      category: ''
-    });
+    
+      const response = await axios.post(
+        'https://crudcrud.com/api/54b7434fe7b8437b854d954f91ddf9c4/products',
+        formData
+      );
+      dispatch({ type: 'ADD_PRODUCT', payload: response.data });
+      setFormData({
+        name: '',
+        description: '',
+        price: '',
+        image: null,
+        category: ''
+      });
+      setShowModal(false);
+    
   };
 
   const handleChange = (e) => {
-    if (e.target.type === 'file') {
-      setformData({ ...formData, image: e.target.files[0] });
-    } else {
-      setformData({ ...formData, [e.target.name]: e.target.value });
-    }
+    const { name, value, type, files } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === 'file' ? files[0] : value
+    }));
   };
 
+  const handleCategoryChange = (e) => {
+    const selectedCategoryId = e.target.value;
+    const selectedCategory = categories.find(
+      (category) => category._id === selectedCategoryId
+    );
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      category: selectedCategory ? selectedCategory.name : ''
+    }));
+  };
 
   return (
     <div>
-    <button className="open-modal-btn" onClick={() => setShowModal(true)}>Create Product</button>
-    {showModal && (
-      <div className="modal">
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder="Product Name"
-          className="modal-input"
-        />
-        <textarea
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-          placeholder="Product Description"
-          className="modal-input"
-        />
-        <input
-          type="number"
-          name="price"
-          value={formData.price}
-          onChange={handleChange}
-          placeholder="Product Price"
-          className="modal-input"
-        />
-        <input
-          type="file"
-          accept="image/*"
-          name="image"
-          onChange={handleChange}
-          className="modal-input"
-        />
-        <select value={formData.category} onChange={handleChange} className="modal-input">
-          <option value="">Select Category</option>
-          {categories.map((category, index) => (
-            <option key={index} value={category._id}>{category.name}</option>
-          ))}
-        </select>
-        <div className="modal-buttons">
-          <button className="save-btn" onClick={handleSave}>Save</button>
-          <button className="cancel-btn" onClick={() => setShowModal(false)}>Cancel</button>
+      <button className="open-modal-btn" onClick={() => setShowModal(true)}>
+        Create Product
+      </button>
+      {showModal && (
+        <div className="modal">
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Product Name"
+            className="modal-input"
+          />
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            placeholder="Product Description"
+            className="modal-input"
+          />
+          <input
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            placeholder="Product Price"
+            className="modal-input"
+          />
+          <input
+            type="file"
+            accept="image/*"
+            name="image"
+            onChange={handleChange}
+            className="modal-input"
+          />
+          {formData.image && (
+            <img
+              src={URL.createObjectURL(formData.image)}
+              alt="Selected Product"
+              className="modal-image"
+            />
+          )}
+          <select
+            value={formData.category}
+            onChange={handleCategoryChange}
+            className="modal-input"
+          >
+            <option value="">Select Category</option>
+            {categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+          <div className="modal-buttons">
+            <button className="save-btn" onClick={handleSave}>
+              Save
+            </button>
+            <button className="cancel-btn" onClick={() => setShowModal(false)}>
+              Cancel
+            </button>
+          </div>
         </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
 };
 
 export default ProductModal;
+
